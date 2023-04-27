1e9dbaa424bbecd85c217bb3ab71657f497e6192
GustavoOlimpio <gustavo-olimpio@hotmail.com>
2023-04-26 22:36:53 -0300

    feat/LoginCadastro

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { signIn, signUp } from "./controllers/authController";
-// import router from "./routes/index.Router.js";
+import router from "./routes/index.Router.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use(router);
+app.use(router);
 
-app.post('/signIn', signIn);
-app.post('/signUp', signUp);
+// mongod --dbpath ~/.mongo
 
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log("server running on port 8000 || or who knows"))
+const PORT = 5000;
+app.listen(PORT, () => console.log("server running on port 5000 || or who knows"))
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,28 @@
-
+import bcrypt  from 'bcrypt'
+import db from '../database/database.connection.js';
+import { v4 as uuid } from 'uuid';
 
 export async function signUp(req, res) {
-
-
+    const {name,email,password} =req.body
+    const hash = bcrypt.hashSync(password, 10);
+    try{
+        await db.collection("signup").insertOne({name,email,password:hash})
+        res.sendStatus(201)
+        } catch (err) {
+        return res.status(500).send(err.message);
+        }
 }
 
 export async function signIn(req, res) {
-
-
+    const {email,password} =req.body
+    try{
+        const user = await db.collection("signup").findOne({ email: email })
+        if (!user) return res.status(404).send("Email nao cadastrado")
+        if (!bcrypt.compareSync(password, user.password)) return res.status(401).send("Senha incorreta, verifique sua senha")
+        const token = uuid();
+        const object = {name:user.name,token:token}
+        res.status(200).send(object)
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
 }
