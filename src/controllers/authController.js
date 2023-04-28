df699d4adf47221ce90fb1caa77b52e5e8bc0420
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-04-28 04:19:24 -0300

    feat:session validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt  from 'bcrypt'
 import db from '../database/database.connection.js';
 import { v4 as uuid } from 'uuid';
+import dayjs from "dayjs";
 
 export async function signUp(req, res) {
     const {name,email,password} =req.body
@@ -20,9 +21,31 @@ export async function signIn(req, res) {
         if (!user) return res.status(404).send("Email nao cadastrado")
         if (!bcrypt.compareSync(password, user.password)) return res.status(401).send("Senha incorreta, verifique sua senha")
         const token = uuid();
-        const object = {name:user.name,token:token}
+        const object = {
+            name:user.name,
+            token:token,
+            idUser: user._id,
+            date: dayjs().format('DD/MM/YY'),
+            exactTime: Date.now()
+        }
+        await db.collection("sessions").insertOne(object)
         res.status(200).send(object)
     } catch (err) {
         return res.status(500).send(err.message);
     }
+}
+
+export async function sessionVerification (req, res) {
+    const {authorization} = req.headers;
+    if(!authorization) return res.status(401).send("unauthorized access");
+    const token = authorization?.replace("Bearer ", "")
+    try{
+        const session = await db.collection("sessions").findOne({token});
+        if(!session) return res.status(401).send("invalid token")
+        const {name, idUser, date, exactTime} = session;
+        res.send(session)
+    } catch (erro) {
+        return res.status(500).send(erro.message)
+    }
+    
 }
--- src/routes/index.Router.js
+++ src/routes/index.Router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signUp,signIn } from "../controllers/authController.js";
+import { signUp,signIn, sessionVerification } from "../controllers/authController.js";
 import { signUpValidation } from "../middlewares/signUpValidation.js";
 import { signInValidation } from "../middlewares/signInValidation.js";
 
 const router=Router();
 router.post('/signIn', signInValidation,signIn);
 router.post('/signUp', signUpValidation,signUp);
+router.post('/', sessionVerification);
 export default router
