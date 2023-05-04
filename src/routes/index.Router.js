53f1a1337d9d8996ef20b5b11f2452cb477d9e5e
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-05-04 03:16:32 -0300

    refac to deploy

--- src/routes/index.Router.js
+++ src/routes/index.Router.js
@@ -7,8 +7,8 @@ import { postProducts } from "../controllers/productsController.js";
 import { productsValidation } from "../middlewares/productsValidation.js";
 
 const router=Router();
-router.post('/signIn', signInValidation,signIn);
-router.post('/signUp', signUpValidation,signUp);
+router.post('/signin', signInValidation,signIn);
+router.post('/signip', signUpValidation,signUp);
 router.get('/products', getProducts);
 router.post('/products',productsValidation,postProducts );
 
